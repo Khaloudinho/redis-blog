796b568950f4f26509820bd417799b26a76aeaf2
Khaloudinho <khaled.bouguettoucha5@etu.univ-lorraine.fr>
2017-11-27 11:51:34 +0100

    updates

--- src/main/java/fr/miage/m2/Article.java
+++ src/main/java/fr/miage/m2/Article.java
@@ -1,8 +1,10 @@
 package fr.miage.m2;
 
+import javafx.util.Pair;
 import redis.clients.jedis.Jedis;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Set;
 
 public class Article {
@@ -53,12 +55,42 @@ public class Article {
         Set<String> articles = conn.zrevrange("nbvotes:article" , 0, 9);
         System.out.println("\n********** Les dix articles ayant reçu le plus de votes sont ci-dessous : ");
         for (String article : articles) System.out.println(article);
-        System.out.println("************************************************************************");
+        System.out.println("*************************************************************************");
         return articles;
     }
 
-    public static double getScoreArticle(Jedis conn, String articleId) {
+    public static double recupererScorePourUnArticle(Jedis conn, String articleId) {
         return conn.zscore("score:article", "article:" + articleId);
     }
 
+    public static void definirCategoriePourUnArticle(Jedis conn, String articleId, String categorie) {
+        conn.sadd("categorie:article:" + articleId, categorie);
+        conn.sadd("categorie:" + categorie, "article:" + articleId);
+    }
+
+    public static void supprimerArticleDeCategorie(Jedis conn, String articleId, String categorie) {
+        conn.srem("categorie:article:" + articleId, categorie);
+        conn.srem("categorie:" + categorie, "article:" + articleId);
+    }
+
+    public static Set<String> recupererArticlesDeCategorie(Jedis conn, String categorie){
+        return conn.smembers("categorie:" + categorie);
+    }
+
+    public static Set<String> recupererCategoriePourUnArticle(Jedis conn, String articleId) {
+        return conn.smembers("categorie:article:" + articleId);
+    }
+
+    public static Set<Pair<String, Double>> recupererScoreDesArticlesDeCategorie(Jedis conn, String categorie) {
+
+        Set<String> articlesDeCategorie = recupererArticlesDeCategorie(conn, categorie);
+
+        Set<Pair<String, Double>> articlesAvecScores = new HashSet<>();
+
+        for (String article : articlesDeCategorie) {
+            articlesAvecScores.add(new Pair<>(article, recupererScorePourUnArticle(conn, article)));
+        }
+
+        return articlesAvecScores;
+    }
 }
--- src/test/java/ArticleTest.java
+++ src/test/java/ArticleTest.java
@@ -1,13 +1,12 @@
 import fr.miage.m2.Article;
+import javafx.util.Pair;
 import org.junit.Test;
 import redis.clients.jedis.Jedis;
-import redis.clients.jedis.Pipeline;
-import redis.clients.jedis.Response;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class ArticleTest {
@@ -32,6 +31,8 @@ public class ArticleTest {
 
         for (int i = 0; i < 20; i++) {
             String articleId = Article.ajoutArticle(conn, utilisateur, titre + i, lien + i);
+            String[] categories = {"programmation", "design", "web", "Java"};
+            Article.definirCategoriePourUnArticle(conn, articleId, categories[(int) Math.floor(Math.random() * 4)]);
             assertEquals(conn.hget("article:" + articleId, "titre"), titre + i);
             assertEquals(conn.hget("article:" + articleId, "lien"), lien + i);
             assertEquals(conn.hget("article:" + articleId, "utilisateur"), utilisateur);
@@ -75,5 +76,127 @@ public class ArticleTest {
         Collections.sort(scoresUn, Collections.reverseOrder());
 
         assertEquals(scoresUn.subList(0, 10), scoresDeux);
+    }
+
+    @Test
+    public void recupererScorePourUnArticleTest(){
+        Jedis conn = new Jedis(host, port);
+
+        String articleId = "2";
+        double unScore = Article.recupererScorePourUnArticle(conn, articleId);
+        double scoreEnBase = conn.zscore("score:article", "article:" + articleId);
+
+        assertEquals(scoreEnBase, unScore, 0);
+    }
+
+    @Test
+    public void definirCategoriePourUnArticleTest(){
+        Jedis conn = new Jedis(host, port);
+        cleanUp();
+
+        String titre = "little article by zerolex";
+        String utilisateur = "zerolex";
+        String lien = "http://www.foo.org/articles/article/1";
+        Article.ajoutArticle(conn, titre, utilisateur, lien);
+
+        String articleId = "1";
+        Article.definirCategoriePourUnArticle(conn, articleId, "new category");
+
+        // Un article appartient à une seule catégorie
+        assertEquals(1, Article.recupererCategoriePourUnArticle(conn, articleId).size());
+
+        // On vérifie qu'il appartient bien à la catégorie qu'on vient de lui assigner
+        assertTrue(Article.recupererCategoriePourUnArticle(conn, articleId).contains("new category"));
+    }
+
+    @Test
+    public void supprimerArticleDeCategorieTest(){
+        Jedis conn = new Jedis(host, port);
+        cleanUp();
+
+        String titre = "little article by zerolex";
+        String utilisateur = "zerolex";
+        String lien = "http://www.foo.org/articles/article/1";
+        Article.ajoutArticle(conn, titre, utilisateur, lien);
+
+        String articleId = "1";
+        String categorie = "new category";
+        Article.definirCategoriePourUnArticle(conn, articleId, categorie);
+
+        // On vérifie que l'article 1 appartient à new category
+        assertTrue(Article.recupererCategoriePourUnArticle(conn, articleId).contains(categorie));
+
+        // On fait la suppression et on vérifie
+        Article.supprimerArticleDeCategorie(conn, articleId, categorie);
+        assertFalse(Article.recupererCategoriePourUnArticle(conn, articleId).contains(categorie));
+    }
+
+    @Test
+    public void recupererArticlesDeCategorieTest(){
+        Jedis conn = new Jedis(host, port);
+        cleanUp();
+
+        String titre = "little article by zerolex - nb ";
+        String utilisateur = "zerolex";
+        String lien = "http://www.foo.org/articles/article/";
+        Article.ajoutArticle(conn, titre + "1", utilisateur, lien + "1");
+        Article.ajoutArticle(conn, titre + "2", utilisateur, lien + "2");
+
+        String categorie = "new category";
+        Article.definirCategoriePourUnArticle(conn, "1", categorie);
+        Article.definirCategoriePourUnArticle(conn, "2", categorie);
+
+        Set<String> articlesDeNewCategory = Article.recupererArticlesDeCategorie(conn, categorie);
+        assertEquals(conn.smembers("categorie:new category"), articlesDeNewCategory);
+    }
+
+    @Test
+    public void recupererCategoriePourUnArticleTest(){
+        Jedis conn = new Jedis(host, port);
+        cleanUp();
+
+        String titre = "little article by zerolex";
+        String utilisateur = "zerolex";
+        String lien = "http://www.foo.org/articles/article/1";
+        Article.ajoutArticle(conn, titre, utilisateur, lien);
+
+        String articleId = "1";
+        String categorie = "new category";
+        Article.definirCategoriePourUnArticle(conn, articleId, categorie);
+
+        // On vérifie qu'on a une seule catégorie pour l'article
+        assertEquals(1, Article.recupererCategoriePourUnArticle(conn, "1").size());
+
+        // On vérifie qu'on a bien récupéré la bonne catégorie
+        assertTrue(Article.recupererCategoriePourUnArticle(conn, "1").contains("new category"));
+    }
+
+    @Test
+    public void recupererScoreDesArticlesDeCategorieTest(){
+        Jedis conn = new Jedis(host, port);
+        cleanUp();
+
+        Set<Pair<String, Double>> articlesEtScores = new HashSet<>();
+
+        String titre = "little article by zerolex - nb ";
+        String utilisateur = "zerolex";
+        String lien = "http://www.foo.org/articles/article/";
+        Article.ajoutArticle(conn, titre + "1", utilisateur, lien + "1");
+        Article.ajoutArticle(conn, titre + "2", utilisateur, lien + "2");
+        Article.ajoutArticle(conn, titre + "3", utilisateur, lien + "3");
+
+        String categorie = "the category";
+        Article.definirCategoriePourUnArticle(conn, "1", categorie);
+        Article.definirCategoriePourUnArticle(conn, "2", categorie);
+        Article.definirCategoriePourUnArticle(conn, "3", categorie);
+
+        Set<String> articleDeTheCategory = Article.recupererArticlesDeCategorie(conn, "the category");
+
+        for (String article : articleDeTheCategory){
+            articlesEtScores.add(new Pair<>(article, conn.zscore("score:article", article)));
+        }
+
+        assertEquals(articlesEtScores, Article.recupererScoreDesArticlesDeCategorie(conn, categorie));
+
     }
 }
